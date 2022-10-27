e81e47388f25175e406e686915d29b249f5bd407
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-10-27 11:20:32 +0200

    day2

---
+++ Queries/Subquery_rownum.sql
@@ -0,0 +1,48 @@
+--how to find employees information of who is making highest salary in the company
+
+--get me the highest salary
+select max(SALARY) from EMPLOYEES;
+
+--highest salary employee information
+select * from EMPLOYEES where SALARY= 24000;
+
+--it calls sub-query
+--sub-query solution in one shot
+select * from EMPLOYEES
+where SALARY = (select max(SALARY) from EMPLOYEES);
+
+--finding second highest salary
+--get highest salary first
+select max(SALARY) from EMPLOYEES;
+
+--highest after 24k
+select max(SALARY) from EMPLOYEES where SALARY <24000;
+
+--one shot combining two queries
+
+select  max(SALARY) from EMPLOYEES where SALARY < (select max (SALARY) from EMPLOYEES);
+
+--employee information of who is making second highest salary?
+
+select * from EMPLOYEES
+where salary = (select max(SALARY) from EMPLOYEES
+where SALARY < (select max (SALARY) from EMPLOYEES));
+
+----------------------------------------------------------
+
+select * from EMPLOYEES;
+
+select * from EMPLOYEES order by salary desc ;
+
+select *from EMPLOYEES where ROWNUM <11;
+
+--list the employees who is making top 10 salary
+
+--get the first 10 people then order them high to low based on salary
+select * from EMPLOYEES where ROWNUM < 11 order by SALARY desc;
+
+--order all employees based on salary high to low then display only first 10 result
+select * from (select * from EMPLOYEES order by salary desc) where ROWNUM <11;
+
+
+
